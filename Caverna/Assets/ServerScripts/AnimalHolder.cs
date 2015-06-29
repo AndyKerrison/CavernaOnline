f995de5f2a9a2fa96902b4755c6451d7ee979eb7
AndyKerrison <adesignforlife@gmail.com>
2015-06-29 21:17:11 +0100

    Some code refactoring

---
+++ Caverna/Assets/ServerScripts/AnimalHolder.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.ServerScripts
+{
+    public class AnimalHolder
+    {
+        public Vector2 position;
+        public bool isCave;
+        public int genericCapacity;
+        public int donkeyCapacity;
+        public int pigCapacity;
+        public int sheepCapacity;
+
+        private int dogs;
+        private int sheep;
+        private int donkeys;
+        private int pigs;
+        private int cows;
+
+        public int RemainingCowCapacity
+        {
+            get
+            {
+                if (sheep > 0 || pigs > 0 || donkeys > 0)
+                    return 0;
+                return genericCapacity - cows;
+            }
+        }
+
+        public int RemainingPigCapacity
+        {
+            get
+            {
+                if (sheep > 0 || cows > 0 || donkeys > 0)
+                    return 0;
+                return Math.Max(genericCapacity, pigCapacity) - pigs;
+            }
+        }
+
+        public int RemainingSheepCapacity
+        {
+            get
+            {
+                if (pigs > 0 || cows > 0 || donkeys > 0)
+                    return 0;
+                return Math.Max(genericCapacity, sheepCapacity) - sheep;
+            }
+        }
+
+        public bool IsUnfilledPigHolder
+        {
+            get { return pigCapacity > pigs; }
+        }
+
+        public bool IsUnfilledDonkeyHolder
+        {
+            get { return donkeyCapacity > donkeys; }
+        }
+
+        public bool IsUnfilledSheepHolder
+        {
+            get { return sheepCapacity > sheep; }
+        }
+
+        public int RemainingDonkeyCapacity
+        {
+            get
+            {
+                if (sheep > 0 || cows > 0 || pigs > 0)
+                    return 0;
+                return Math.Max(genericCapacity, donkeyCapacity) - donkeys;
+            }
+        }
+
+        public string TileType { get; set; }
+
+        public void FillAnimals(string animalType, ref int animalsToAllocate)
+        {
+            int remainingCapacity = 0;
+            if (animalType == ResourceTypes.Cows)
+                remainingCapacity = RemainingCowCapacity;
+            if (animalType == ResourceTypes.Pigs)
+                remainingCapacity = RemainingPigCapacity;
+            if (animalType == ResourceTypes.Donkeys)
+                remainingCapacity = RemainingDonkeyCapacity;
+            if (animalType == ResourceTypes.Sheep)
+                remainingCapacity = RemainingSheepCapacity;
+            if (animalType == ResourceTypes.Dogs)
+                remainingCapacity = 1000;
+
+            int animalsToAdd = Math.Min(remainingCapacity, animalsToAllocate);
+
+            if (animalType == ResourceTypes.Cows)
+                cows += animalsToAdd;
+            if (animalType == ResourceTypes.Pigs)
+                pigs += animalsToAdd;
+            if (animalType == ResourceTypes.Donkeys)
+                donkeys += animalsToAdd;
+            if (animalType == ResourceTypes.Sheep)
+                sheep += animalsToAdd;
+            if (animalType == ResourceTypes.Dogs)
+                dogs += animalsToAdd;
+
+            if (!isCave)
+                sheepCapacity = dogs + 1;
+
+            animalsToAllocate -= animalsToAdd;
+        }
+
+        public void AddDog(ref int dogsToAllocate)
+        {
+            if (dogsToAllocate <= 0)
+                return;
+            dogs++;
+            dogsToAllocate--;
+            if (!isCave)
+                sheepCapacity = dogs + 1;
+        }
+
+        public int GetDogs()
+        {
+            return dogs;
+        }
+
+        public int GetSheep()
+        {
+            return sheep;
+        }
+
+        public int GetDonkeys()
+        {
+            return donkeys;
+        }
+
+        public int GetPigs()
+        {
+            return pigs;
+        }
+
+        public int GetCows()
+        {
+            return cows;
+        }
+    }
+}
--- Caverna/Assets/UIScripts/SideBarPanelScript.cs
+++ Caverna/Assets/UIScripts/SideBarPanelScript.cs
@@ -1,94 +1,96 @@
-﻿using Assets.UIScripts;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SideBarPanelScript : MonoBehaviour
+namespace Assets.UIScripts
 {
-    private GameObject _woodCount;
-    private GameObject _oreCount;
-    private GameObject _stoneCount;
-    private GameObject _foodCount;
-    private GameObject _goldCount;
-    private GameObject _grainCount;
-    private GameObject _vegCount;
-    private GameObject _rubyCount;
-    private GameObject _beggingCount;
-    private GameObject _scoreCount;
+    public class SideBarPanelScript : MonoBehaviour
+    {
+        private GameObject _woodCount;
+        private GameObject _oreCount;
+        private GameObject _stoneCount;
+        private GameObject _foodCount;
+        private GameObject _goldCount;
+        private GameObject _grainCount;
+        private GameObject _vegCount;
+        private GameObject _rubyCount;
+        private GameObject _beggingCount;
+        private GameObject _scoreCount;
     
-	// Use this for initialization
-	void Start ()
-	{
-        var parentWidth = transform.GetComponent<RectTransform>().rect.width;
-        var parentHeight = transform.GetComponent<RectTransform>().rect.height;
-        var spriteSize = new Vector2(parentWidth / 1.2f, parentHeight / 8f);
-	    var verticalIncrement = parentHeight/11f;
-        var verticalOffset = transform.GetComponent<RectTransform>().anchoredPosition.y + parentHeight / 20f;
-	    var horizontalOffset = transform.GetComponent<RectTransform>().anchoredPosition.x - parentWidth/10f;
+        // Use this for initialization
+        void Start ()
+        {
+            var parentWidth = transform.GetComponent<RectTransform>().rect.width;
+            var parentHeight = transform.GetComponent<RectTransform>().rect.height;
+            var spriteSize = new Vector2(parentWidth / 1.2f, parentHeight / 8f);
+            var verticalIncrement = parentHeight/11f;
+            var verticalOffset = transform.GetComponent<RectTransform>().anchoredPosition.y + parentHeight / 20f;
+            var horizontalOffset = transform.GetComponent<RectTransform>().anchoredPosition.x - parentWidth/10f;
 
-	    _woodCount = InitIcon(ResourceTypes.Wood, spriteSize);
-        _oreCount = InitIcon(ResourceTypes.Ore, spriteSize);
-        _stoneCount = InitIcon(ResourceTypes.Stone, spriteSize);
-        _foodCount = InitIcon(ResourceTypes.Food, spriteSize);
-        _goldCount = InitIcon(ResourceTypes.Gold, spriteSize);
-        _grainCount = InitIcon(ResourceTypes.Grain, spriteSize);
-        _vegCount = InitIcon(ResourceTypes.Veg, spriteSize);
-        _rubyCount = InitIcon(ResourceTypes.Ruby, spriteSize);
-        _beggingCount = InitIcon(ResourceTypes.Begging, spriteSize);
-        _scoreCount = InitIcon(ResourceTypes.ScoreMarker, spriteSize);
+            _woodCount = InitIcon(ResourceTypes.Wood, spriteSize);
+            _oreCount = InitIcon(ResourceTypes.Ore, spriteSize);
+            _stoneCount = InitIcon(ResourceTypes.Stone, spriteSize);
+            _foodCount = InitIcon(ResourceTypes.Food, spriteSize);
+            _goldCount = InitIcon(ResourceTypes.Gold, spriteSize);
+            _grainCount = InitIcon(ResourceTypes.Grain, spriteSize);
+            _vegCount = InitIcon(ResourceTypes.Veg, spriteSize);
+            _rubyCount = InitIcon(ResourceTypes.Ruby, spriteSize);
+            _beggingCount = InitIcon(ResourceTypes.Begging, spriteSize);
+            _scoreCount = InitIcon(ResourceTypes.ScoreMarker, spriteSize);
 
-	    _woodCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset);
-        _oreCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - verticalIncrement);
-        _stoneCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 2 * verticalIncrement);
-        _foodCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 3 * verticalIncrement);
-        _goldCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 4 * verticalIncrement);
-        _grainCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 5 * verticalIncrement);
-        _vegCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 6 * verticalIncrement);
-        _rubyCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 7 * verticalIncrement);
-        _beggingCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 8 * verticalIncrement);
-        _scoreCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 9 * verticalIncrement);
-    }
+            _woodCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset);
+            _oreCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - verticalIncrement);
+            _stoneCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 2 * verticalIncrement);
+            _foodCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 3 * verticalIncrement);
+            _goldCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 4 * verticalIncrement);
+            _grainCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 5 * verticalIncrement);
+            _vegCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 6 * verticalIncrement);
+            _rubyCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 7 * verticalIncrement);
+            _beggingCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 8 * verticalIncrement);
+            _scoreCount.GetComponent<RectTransform>().anchoredPosition = new Vector2(horizontalOffset, verticalOffset - 9 * verticalIncrement);
+        }
 
-    private GameObject InitIcon(string resourceType, Vector2 spriteSize)
-    {
-        GameObject icon = ResourceIcon.Create(resourceType, true);
-        icon.transform.SetParent(transform, true);
-        icon.transform.localScale = transform.localScale;
-        icon.transform.position = transform.position;
-        spriteSize.y = (spriteSize.x/icon.GetComponent<RectTransform>().sizeDelta.x)*
-                       icon.GetComponent<RectTransform>().sizeDelta.y;
-        icon.GetComponent<RectTransform>().sizeDelta = spriteSize;
-        return icon;
-    }
+        private GameObject InitIcon(string resourceType, Vector2 spriteSize)
+        {
+            GameObject icon = ResourceIcon.Create(resourceType, true);
+            icon.transform.SetParent(transform, true);
+            icon.transform.localScale = transform.localScale;
+            icon.transform.position = transform.position;
+            spriteSize.y = (spriteSize.x/icon.GetComponent<RectTransform>().sizeDelta.x)*
+                           icon.GetComponent<RectTransform>().sizeDelta.y;
+            icon.GetComponent<RectTransform>().sizeDelta = spriteSize;
+            return icon;
+        }
 
-    public void SetResources(string resourceName, int count)
-    {
-        if (resourceName == ResourceTypes.Wood)
-            _woodCount.GetComponent<ResourceIcon>().SetValue(count);
+        public void SetResources(string resourceName, int count)
+        {
+            if (resourceName == ResourceTypes.Wood)
+                _woodCount.GetComponent<ResourceIcon>().SetValue(count);
 
-        if (resourceName == ResourceTypes.Ore)
-            _oreCount.GetComponent<ResourceIcon>().SetValue(count);
+            if (resourceName == ResourceTypes.Ore)
+                _oreCount.GetComponent<ResourceIcon>().SetValue(count);
 
-        if (resourceName == ResourceTypes.Stone)
-            _stoneCount.GetComponent<ResourceIcon>().SetValue(count);
+            if (resourceName == ResourceTypes.Stone)
+                _stoneCount.GetComponent<ResourceIcon>().SetValue(count);
 
-        if (resourceName == ResourceTypes.Food)
-            _foodCount.GetComponent<ResourceIcon>().SetValue(count);
+            if (resourceName == ResourceTypes.Food)
+                _foodCount.GetComponent<ResourceIcon>().SetValue(count);
 
-        if (resourceName == ResourceTypes.Gold)
-            _goldCount.GetComponent<ResourceIcon>().SetValue(count);
+            if (resourceName == ResourceTypes.Gold)
+                _goldCount.GetComponent<ResourceIcon>().SetValue(count);
 
-        if (resourceName == ResourceTypes.Grain)
-            _grainCount.GetComponent<ResourceIcon>().SetValue(count);
+            if (resourceName == ResourceTypes.Grain)
+                _grainCount.GetComponent<ResourceIcon>().SetValue(count);
 
-        if (resourceName == ResourceTypes.Veg)
-            _vegCount.GetComponent<ResourceIcon>().SetValue(count);
+            if (resourceName == ResourceTypes.Veg)
+                _vegCount.GetComponent<ResourceIcon>().SetValue(count);
 
-        if (resourceName == ResourceTypes.Ruby)
-            _rubyCount.GetComponent<ResourceIcon>().SetValue(count);
+            if (resourceName == ResourceTypes.Ruby)
+                _rubyCount.GetComponent<ResourceIcon>().SetValue(count);
 
-        if (resourceName == ResourceTypes.Begging)
-            _beggingCount.GetComponent<ResourceIcon>().SetValue(count);
+            if (resourceName == ResourceTypes.Begging)
+                _beggingCount.GetComponent<ResourceIcon>().SetValue(count);
 
-        if (resourceName == ResourceTypes.ScoreMarker)
-            _scoreCount.GetComponent<ResourceIcon>().SetValue(count);
+            if (resourceName == ResourceTypes.ScoreMarker)
+                _scoreCount.GetComponent<ResourceIcon>().SetValue(count);
+        }
     }
 }
